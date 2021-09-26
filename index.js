5d9337ea7e9b328bb36dd26923a2127eddeed6be
BinhPham <cbinh951>
2021-09-26 21:25:02 +0700

    add code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World 12344 new!');
+  res.send('Hello World 12344 new aa!');
 });
 
 var server = app.listen(3000, function () {
