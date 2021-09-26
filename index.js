621fec58276dc69a82fede0aaa2039d85713671c
BinhPham <cbinh951>
2021-09-26 22:21:01 +0700

    update jenkins file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World 12344 new aa 1!');
+  res.send('Hello World 12344 new aa 122!');
 });
 
 var server = app.listen(3000, function () {
